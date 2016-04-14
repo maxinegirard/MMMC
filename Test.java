d2fe3a0e6b799c43629571beccbb65835f9afc16
yeagerc <yeagerc@simmons.edu>
2016-04-14 10:03:34 -0400

    Added files via upload
    This is for the pop up, gives little Information graphic, then press okay for gameplay

---
+++ Test.java
@@ -0,0 +1,6 @@
+import javax.swing.JOptionPane;
+class Test{
+   public static void main(String argv[]){
+      JOptionPane.showMessageDialog(null, "This is a matching game.  Find all the matches before the time runs out!", "Gameplay", JOptionPane.INFORMATION_MESSAGE);
+   }
+}
